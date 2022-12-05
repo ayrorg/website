863bceeb313701fa9e35d5dac3784ff3e69717ac
moripen <markusmoripen@gmail.com>
2022-12-05 15:58:18 +0100

    feat: courses page
    * feat: courses page

---
+++ src/pages/courses.tsx
@@ -0,0 +1,32 @@
+import { NextPage } from "next";
+import { Box, Container, Flex, Grid, Image, Link, Paragraph } from "theme-ui";
+import { Layout } from "../components/layout/layout";
+
+const Courses: NextPage = () => {
+  return (
+    <Layout>
+      <Grid sx={{ width: "100%", px: 4 }}>
+        <Container>
+            
+            <Paragraph sx={{
+                fontSize: "25px",
+                fontWeight: "400",
+                textAlign: "center"
+            }}>
+                "AYR guidet meg gjennom grunnleggende verktøy for kommunikasjon og samarbeid 
+                på tvers av hele arbeidsflaten. Et verdifullt kurs som jeg kan anbefale videre"
+            </Paragraph>
+            <Paragraph sx={{
+                fontSize: "10px",
+                fontWeight: "300",
+                textAlign: "right"
+            }}> - Janne Syversen, Miles
+            </Paragraph>
+        </Container>
+        </Grid>
+        </Layout>
+
+  )
+};
+
+export default Courses;
